02de112b51a248c4088e6f35bff5f9629ad12035
kiku_shonan <kiku.shonan.git@gmail.com>
2019-08-18 22:14:26 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 菊村誠一 on 2019/08/18.
+//  Copyright © 2019 kiku.shonan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
